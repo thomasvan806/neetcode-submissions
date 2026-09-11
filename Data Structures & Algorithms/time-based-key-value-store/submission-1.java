8b05d4ebeab56bf438ed86fba500774462bccf62
thomasvan806 <thomasvan806@gmail.com>
2026-09-11 16:42:37 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,47 @@
+class TimeMap {
+    Map<String, List<Integer>> timeMap;
+    Map<Integer, String> timestampToValue;
+
+    public TimeMap() {
+        timeMap = new HashMap<>();
+        timestampToValue = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        List<Integer> timestamps;
+        if (!timeMap.containsKey(key)) {
+            timestamps = new ArrayList<>();
+            timeMap.put(key, timestamps);
+        } else {
+            timestamps = timeMap.get(key);
+        }
+        timestamps.add(timestamp);
+        timestampToValue.put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!timeMap.containsKey(key)) return "";
+
+        List<Integer> timestamps = timeMap.get(key);
+        
+        int min = 0;
+        int max = timestamps.size() - 1;
+        int mid = (min + max) / 2;
+
+        while (min <= max) {
+            int curr = timestamps.get(mid);
+            if (curr == timestamp) return timestampToValue.get(curr);
+
+            if (curr < timestamp) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+
+            mid = (min + max) / 2;
+        }
+
+        int curr = timestamps.get(mid);
+        return curr > timestamp ? "" : timestampToValue.get(curr);
+    }
+}
